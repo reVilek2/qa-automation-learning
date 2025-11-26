daf9e7c29a11c5011f7e7367ea32852b37769b16
reVilek2 <reVilek123@gmail.com>
2025-11-26 15:09:34 +0400

    feat: день 1 — переменные, let/const, console.log

---
+++ 01-base-start/app.js
@@ -0,0 +1,41 @@
+let num = 42 //number
+let firstName = 'Alex' //string
+const isProgramemr = true // boolean
+
+/**  Can do
+let $ = 'test'
+let $num = 42
+let num$ = 42
+let _ = 49
+let _num = 42
+let num_ = 12
+let myNum = 34
+let num42 = 10
+*/
+
+/** Restricted
+let first_name = 'elena'
+let 42num = '11'
+let my-num = 1
+let false
+*/
+
+//alert (firstName)
+//console.log('test:', firstName, isProgramemr)
+//console.log(num + 10)
+//console.log(num - 10)
+//console.log(num * 10)
+//console.log(num / 10)
+//console.log(num)
+
+//let num2 = num + 10
+//console.log(num, num2)
+//num = num2 - num 
+//num2 = num2 + 1
+//console.log (num, num2)
+
+//let num3 = (num + 10) * 2 / (5 - 1)
+//console.log(num3)
+
+const fullName = firstName + ' ' + 'Obuhov'
+console.log(fullName)
